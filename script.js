dcd81c6b24d3023f305caeb657e315e760f7dcd9
lokrisx3 <cfritzsepulveda8@gmail.com>
2026-05-27 14:56:49 -0400

    Se crea H5 con funcion de color aleatorio

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function aleatorio(){
-    let color = ["Azul","Rojo","Blanco"]
+function aleatorio() {
+    let color = ["Azul", "Rojo", "Blanco"]
     let azar = Math.floor(Math.random() * color.length);
-    let seleccion= color[azar]
+    let seleccion = color[azar]
     return seleccion;
 }
 
 console.log(aleatorio())
+
+let etiquetasH5 = document.querySelectorAll("h5");
+let coloresCss = {
+    Azul: "blue",
+    Rojo: "red",
+    Blanco: "white"
+};
+
+etiquetasH5.forEach(function (etiqueta) {
+    etiqueta.addEventListener("click", function () {
+        let color = aleatorio();
+        etiqueta.style.color = coloresCss[color];
+    });
+});
