bc8dea795bf4d312cc0d3f497055618a610657fd
lokrisx3 <cfritzsepulveda8@gmail.com>
2026-05-27 16:31:17 -0400

    se crea footer en un script

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ etiquetasH5.forEach(function (etiqueta) {
         etiqueta.style.color = coloresCss[color];
     });
 });
+
+function cargarFooter() {
+    let contenedorFooter = document.querySelector("#footer-container");
+    let autores = [
+        "Cristian Fritz",
+        "Catalina Olave",
+        "Maria de los angeles farias",
+        "Gustavo Soto"
+    ];
+    let listaAutores = "";
+
+    autores.forEach(function (autor) {
+        listaAutores += `<li class="nav-item"><span class="nav-link px-0 px-sm-2 text-body-secondary">${autor}</span></li>`;
+    });
+
+    if (contenedorFooter) {
+        contenedorFooter.innerHTML = `
+            <div class="container">
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <p class="col-md-4 mb-0 text-body-secondary">&copy; 2025 ABC-Challenge</p>
+
+                    <a href="index.html" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="ABC-Challenge">
+                        <h5 class="mb-0">ABC-Challenge</h5>
+                    </a>
+
+                    <div class="col-md-4 text-md-end">
+                        <a href="mailto:contacto@abc-challenge.cl" class="d-block mb-2 link-secondary text-decoration-none">contacto@abc-challenge.cl</a>
+                        <ul class="nav justify-content-md-end justify-content-start flex-column flex-sm-row flex-wrap">
+                            ${listaAutores}
+                        </ul>
+                    </div>
+                </footer>
+            </div>
+        `;
+    }
+}
+
+cargarFooter();
